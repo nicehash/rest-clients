3f632cb27bac3bd8b300e76744bb209e0f1786c5
object99 <alan.hablak@nicehash.com>
2024-09-18 12:03:23 +0200

    added new property TOKEN PROVIDER URL

--- common/src/main/java/com/nicehash/clients/common/spi/Options.java
+++ common/src/main/java/com/nicehash/clients/common/spi/Options.java
@@ -113,4 +113,9 @@ public class Options {
      * Password
      */
     public static final Option<String> PASSWORD = Option.simple(Options.class, "PASSWORD", String.class, true);
+
+    /**
+     * Token provider url
+     */
+    public static final Option<String> TOKEN_PROVIDER_URL = Option.simple(Options.class, "TOKEN_PROVIDER_URL", String.class, true);
 }
